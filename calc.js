89741d96ec870ab3327e96271d32dba1dc10e98b
Tsheung <tobylsheung@gmail.com>
2022-01-21 13:09:02 -0500

    Special cases

--- calc.js
+++ calc.js
@@ -38,6 +38,7 @@ let action;
 //Obtains display Number
 const calculate = document.querySelector(".mainNum");
 
+
 //Click Reaction to Only Numbers
 const numerical = document.querySelectorAll(".num");
 numerical.forEach((n) => {
@@ -137,6 +138,35 @@ math.forEach((m) => {
 //C to clear everything
 //Click only once for decimal
 
+const special = document.querySelectorAll(".sp");
+special.forEach((s) => {
+    s.addEventListener("click", () => {
+        if(s.innerHTML == "C") {
+            displayNum = 0;
+            memoryNum = 0;
+            calculate.innerHTML = displayNum;
+        } else if(s.innerHTML == "CE") {
+            displayNum = 0;
+            calculate.innerHTML = displayNum;
+        } else if(s.innerHTML == "-/+") {
+            if(displayNum.includes("-")){
+                displayNum = displayNum.substring(1);
+                calculate.innerHTML = displayNum;
+            } else {
+                displayNum = "-" + displayNum;
+                calculate.innerHTML = displayNum;
+            }
+        } else if(s.innerHTML == "%") {
+            displayNum = displayNum / 100;
+            calculate.innerHTML = displayNum;
+        } else if (s.innerHTML == ".") {
+            if(!displayNum.includes(".")) {
+                displayNum = displayNum + ".";
+                calculate.innerHTML = displayNum;
+            }
+        }
 
+    })
+})
 
 
