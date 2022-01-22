e9648039073ce822e2f0bbdda391f6e3b0a90631
Tsheung <39866613+Tsheung@users.noreply.github.com>
2022-01-21 22:50:46 -0500

    Update Image link reference

--- calc.js
+++ calc.js
@@ -165,7 +165,7 @@ hoverButton.forEach((h) => {
 const banner = document.createElement('div');
 banner.classList.add("github");
 const image = document.createElement('img');
-image.src = "Images/github.png"
+image.src = "Images/Github.png"
 image.setAttribute("alt", "Github logo that links to my github");
 image.addEventListener("click", () => {
     window.open("https://github.com/Tsheung/Calculator", "_blank");
